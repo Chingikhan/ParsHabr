c882dcad9b1c1eb96e02dc0608a8b995b9226f26
Chingikhan <122561568+Chingikhan@users.noreply.github.com>
2023-04-18 19:54:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import requests
 import json
 
 post_id = 727758
-url = f"https://m.habr.com/kek/v2/articles/{post_id}"  # замените на реальный URL API
+url = f"https://m.habr.com/kek/v2/articles/{post_id}"
 
-# Опционально: установить заголовки и параметры запроса, если это требуется API
-headers = {'Authorization': 'Bearer <TOKEN>'}  # замените на реальный токен, если это требуется API
-params = {'param1': 'value1', 'param2': 'value2'}  # замените на реальные параметры, если это требуется API
+headers = {'Authorization': 'Bearer <TOKEN>'}
+params = {'param1': 'value1', 'param2': 'value2'}
 
-# Отправить GET-запрос и получить ответ в формате JSON
 response = requests.get(url, headers=headers, params=params)
 data = json.loads(response.text)
 
-# Получение всех ключей словаря
 keys = data.keys()
 
-# Печать всех ключей
 for key in keys:
     print(key)
